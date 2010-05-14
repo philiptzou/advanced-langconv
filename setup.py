42f9b14a611b03cadec19f6f022229851d61c562
liangent <liangent@0ff07d6f-c699-b587-8cb4-38489f51b34c>
2010-05-14 12:53:29 +0000

    Define setup.py
    git-svn-id: http://advanced-langconv.googlecode.com/svn/trunk@10 0ff07d6f-c699-b587-8cb4-38489f51b34c

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@
 
 from distutils.core import setup, Extension
 
-converter = Extension('converter', sources = ['converter.c'])
+converter = Extension('langconv.converter', sources = ['langconv/converter.c'])
 
-setup (name = 'AdvancedLangConv', version = '0.01', description = 'Advanced Language Converter', ext_modules = [converter]) 
+setup(
+	name = 'AdvancedLangConv',
+	version = '0.01',
+	description = 'Advanced Language Converter',
+	url = 'http://code.google.com/p/advanced-langconv/',
+	packages = ['langconv'],
+	ext_modules = [converter]
+) 
